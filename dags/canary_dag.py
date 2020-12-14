5889c0d5b07ba8575258f41e1768ed9fe19797dd
thibault ketterer <thekit@gmail.com>
2020-12-14 15:19:15 +0100

    initial commit

---
+++ dags/canary_dag.py
@@ -0,0 +1,32 @@
+# mandatory dag for prometheus exporter and airflow self-check
+#
+from datetime import timedelta, datetime
+
+from airflow import DAG
+
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+        "owner": "quark",
+        "depends_on_past": False,
+        "email_on_failure": False,
+        }
+
+with DAG('canary_dag',
+         start_date=datetime(2020, 10, 8),
+         default_args=default_args,
+         tags=["monitoring", "quark"],
+         max_active_runs=1,
+         concurrency=1,
+         schedule_interval=timedelta(minutes=1),
+         catchup=False,
+         ) as dag:
+    BashOperator(
+        task_id='check_alive',
+        bash_command='''date ;
+        date +%s ;
+        date > /var/log/airflow/airflow_last_schedule;
+        date +%s >> /var/log/airflow/airflow_last_schedule
+        ''',
+        retries=0,
+    ).doc_md = 'task is to check airflow is able to schedule'
